efdd7a2b2f470c2d74e365e1d3843af8b9852b01
Joohyung Park <joohyung.park@on-the-ground.org>
2025-04-18 05:20:32 +0000

    enhance(test_helper): refactor TestLogEffectHandler to log out via stdout

--- effects/helpers_test.go
+++ effects/helpers_test.go
@@ -2,19 +2,24 @@ package effects_test
 
 import (
 	"context"
+	"os"
 
 	"github.com/on-the-ground/effect_ive_go/effects"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zaptest/observer"
+	"go.uber.org/zap/zapcore"
 )
 
 func WithTestLogEffectHandler(
 	ctx context.Context,
 ) (context.Context, func() context.Context) {
-	core, _ := observer.New(zap.DebugLevel)
+	consoleCore := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig()),
+		zapcore.Lock(os.Stdout),
+		zap.DebugLevel,
+	)
 	return effects.WithZapLogEffectHandler(
 		ctx,
 		1,
-		zap.New(core),
+		zap.New(consoleCore),
 	)
 }
